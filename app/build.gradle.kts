3f948605da4c51527acbddcc084bd24f1d7f9de9
Sohaib Ouakani <sohaib.ouakani@studio.unibo.it>
2026-05-24 12:33:24 +0200

    chore: aligned jvm versions to TravelDiary + add navigation.compose and material icons extended dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,18 +1,22 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
     alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.kotlin.serialization)
 }
 
 android {
     namespace = "com.example.corsa"
     compileSdk {
-        version = release(37)
+        version = release(36)
     }
 
     defaultConfig {
         applicationId = "com.example.corsa"
         minSdk = 26
-        targetSdk = 37
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
@@ -30,10 +34,9 @@ android {
     }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_21
-        targetCompatibility = JavaVersion.VERSION_21
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
     }
-
     buildFeatures {
         compose = true
     }
@@ -41,7 +44,7 @@ android {
 
 kotlin {
     compilerOptions {
-        jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_21
+        jvmTarget = JvmTarget.JVM_11
     }
 }
 
@@ -73,4 +76,9 @@ dependencies {
 
     // Jetpack Compose (if not already present)
     implementation(libs.compose.auth.ui)
+
+    // Copied dependencies from Travel Diary
+    //Navigation
+    implementation(libs.androidx.navigation.compose)
+    implementation(libs.androidx.material.icons.extended)
 }
